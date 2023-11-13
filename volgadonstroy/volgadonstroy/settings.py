ae0668898a2736e2173f31241fd20b2257ef4a24
Alexey Derendyaev <alexderendyaev@yandex.ru>
2023-11-13 17:13:42 -0300

    better than previous!

--- volgadonstroy/volgadonstroy/settings.py
+++ volgadonstroy/volgadonstroy/settings.py
@@ -165,11 +165,11 @@ CACHES = {
     }
 }
 
-EMAIL_HOST = 'smtp.timeweb.ru'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'feedback@djangosite.tw1.ru'
-DEFAULT_FROM_EMAIL = 'feedback@djangosite.tw1.ru'
-SERVER_EMAIL = 'feedback@djangosite.tw1.ru'
-EMAIL_HOST_PASSWORD = 'RqjA67B7'
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = os.getenv('SERVER_EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
